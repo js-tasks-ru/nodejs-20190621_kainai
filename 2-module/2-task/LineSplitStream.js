b81fc8463c215c998d4697b7e1bc1c46bae088f6
Alevtine <mirada.insolente@gmail.com>
2019-06-30 15:56:50 +0300

    add solution 2-module 2-task

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -4,12 +4,23 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.line = '';
   }
 
   _transform(chunk, encoding, callback) {
+    this.line += chunk;
+    this.splitLine();
+    callback();
+  }
+
+  splitLine() {
+    const lines = this.line.split(`${os.EOL}`);
+    lines.map((item, idx) => idx === lines.length - 1 ? this.line = item : this.push(item));
   }
 
   _flush(callback) {
+    this.push(this.line);
+    callback();
   }
 }
 
