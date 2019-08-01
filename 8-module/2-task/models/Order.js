a0fec6234a333bc2fa7143f583844d9f34c921c1
Alevtine <mirada.insolente@gmail.com>
2019-08-01 17:57:43 +0300

    add solution 8-module 2-task

--- 8-module/2-task/models/Order.js
+++ 8-module/2-task/models/Order.js
@@ -2,6 +2,32 @@ const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
 const orderSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+    ref: 'User',
+  },
+  product: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+    ref: 'Product',
+  },
+  phone: {
+    type: String,
+    required: true,
+    validate: [
+      {
+        validator(value) {
+          return /\+?\d{6,14}/.test(value);
+        },
+        message: 'invalid phone',
+      },
+    ],
+  },
+  address: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = connection.model('Order', orderSchema);
